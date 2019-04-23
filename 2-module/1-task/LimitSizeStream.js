87b3a8f7bafc603696b6e6f3097b4cf3b74231ed
orlovvalentino@gmail.com <orlovvalentino@gmail.com>
2019-04-23 10:18:14 +0300

    add solution 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -5,14 +5,17 @@ class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
     this.limit = options.limit;
-    this.size =0;
+    this.currSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
-    if((this.size += chunk.length) > this.limit){
-      callback(new LimitExceededError, null)
+    this.currSize += chunk.length;
+    if (this.currSize > this.limit) {
+      callback(new LimitExceededError(), null);
+      return false;
     }
-      callback(null, chunk)
+    callback(null, chunk);
+
   }
 }
 
